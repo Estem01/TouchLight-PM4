88d6597fbd2637a75d5b9a25a6e4c9ec8a366564
HenryDM <100979680+Henry12960@users.noreply.github.com>
2022-08-16 17:59:19 -0300

    Add files via upload

---
+++ src/HenryDM/TouchLight/Main.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace HenryDM\TouchLight;
+
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\block\Block;
+
+class Main extends PluginBase implements Listener {
+
+    public function onEnable() : void 
+	{
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onPlayerInteract(PlayerInteractEvent $ev){
+        $block = $event->getBlock();
+        if($event->getAction() === PlayerInteractEvent::RIGHT_CLICK_BLOCK){
+            switch($block->getId()){
+                case 123:
+                    $block->getWorld()->setBlock($block->asVector3(), Block::get(124));
+                break;
+                case 124:
+                    $block->getWorld()->setBlock($block->asVector3(), Block::get(123));
+                break;
+            }
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
